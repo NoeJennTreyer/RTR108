4c73920e86d41fce77a5b29e5bcd378124632b2c
NoeJennTreyer <noe.jenntreyer@gmail.com>
2020-02-06 09:27:07 +0200

    first commit

---
+++ test01.py
@@ -0,0 +1,70 @@
+Python 3.6.8 (default, Jan 14 2019, 11:02:34) 
+[GCC 8.0.1 20180414 (experimental) [trunk revision 259383]] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======================== RESTART: /home/user/test.py ========================
+{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/user/test.py'}
+{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/user/test.py', 'a': 10}
+>>> 
+======================== RESTART: /home/user/test.py ========================
+4
+4
+>>> 
+======================== RESTART: /home/user/test.py ========================
+Welcome to the wonderful world of Pokepython
+First of all, what is your name?desuvult
+Have a good day in this funny land,  desuvult
+>>> 
+======================== RESTART: /home/user/test.py ========================
+Welcome to the wonderful world of Pokepython
+First of all, what is your name?GrandpaSmurf
+Have a good day in this funny land,  GrandpaSmurf !
+Prof. Latvia: Choose your starter!
+1: Regard
+2: Vide
+Choose between these two Pokemons:2
+Your pokemon is:  V
+>>> 
+======================== RESTART: /home/user/test.py ========================
+Welcome to the wonderful world of Pokepython
+First of all, what is your name?d
+Have a good day in this funny land,  d !
+Prof. Latvia: Choose your starter!
+1: Regard
+2: Vide
+Choose between these two Pokemons:1
+Your pokemon is:  Vide
+>>> 
+======================== RESTART: /home/user/test.py ========================
+Welcome to the wonderful world of Pokepython
+First of all, what is your name?j
+Have a good day in this funny land,  j !
+Prof. Latvia: Choose your starter!
+1: Regard
+2: Vide
+Choose between these two Pokemons:1
+Your pokemon is:  ['Vide', 9, 10, 9, 9]
+>>> 
+======================== RESTART: /home/user/test.py ========================
+Welcome to the wonderful world of Pokepython
+First of all, what is your name?j
+Have a good day in this funny land,  j !
+Prof. Latvia: Choose your starter!
+1: Regard
+2: Vide
+Choose between these two Pokemons:2
+Your pokemon is:  ['Vide', 9, 10, 9, 9]
+>>> 
+======================== RESTART: /home/user/test.py ========================
+Welcome to the wonderful world of Pokepython
+First of all, what is your name?k
+Have a good day in this funny land,  k !
+Prof. Latvia: Choose your starter!
+1: Regard
+2: Vide
+Choose between these two Pokemons:k
+Traceback (most recent call last):
+  File "/home/user/test.py", line 13, in <module>
+    team+=test02
+NameError: name 'test02' is not defined
+>>> 
